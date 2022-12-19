90267ff1d6f3ad4a31a8236ce0758c7bfb046f61
Avishka Indula <avishkainduladev@gmail.com>
2022-12-19 15:23:56 +0530

    Adding Routing
    => npm install --save react-router-dom@5
    => In react, there's only one html page
    => but we can have it load different pages
    *  for different urls.
    => react router dom package will help for that.
    => Those new pages are also components.
    => but we add those components into a separate folder called
    *  pages instead of adding them to components folder.
    => creating AllMeetups.js , Favorites.js and NewMeetup.js files.
    => importing { BrowserRouter } to index.js file and wrapping < App />
    *  component inside <BrowserRouter> component.
    => import { Route } component to App.js
    => importing the 3 pages to App.js
    => Adding Route components to App() function and define the
    *  routes for different pages.
    => adding Switch beside { Route } import and wrap the Route components
    *  inside it.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favourites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/* this paths are the urls like localhost:3000/new-meetup */}
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+      {/* If we don't wrap the routes from switch, when we */}
+      {/* enter "/favorites" as url, the content of both "/" and "/favorites" */}
+      {/* will be displayed. To prevent that, we need to wrap the */}
+      {/* urls inside Switch and add "exact" prop to the "/" path */}
+    </div>
+  );
 }
 
 export default App;
