9e0f6f745ada43dcdf6c4b8ffbb3994facd1a392
Avishka Indula <avishkainduladev@gmail.com>
2022-12-21 17:52:25 +0530

    Getting Started with Fetching Data
    => adding a fetch() function in AllMeetupsPage() function of AllMeetups.js
    => by default, fetch will send a get request. So we don't wanna
    *  configure it here.
    => Now convert the data into a JS object by using response.json
    => as response.json returns a promise, we need to add .then to
    *  it and get the actual data
    => But now we have a problem because fetch() returns a promise,
    *  but we should wait until that function finished executing
    *  before return() the jsx elements.
    => we can't use async await and await fetch(); but we can't do
    *  that because we can't add async to function AllMeetupsPage() {}
    *  because react components must be synchronous.
    => So we need to return some temporary jsx code (like a loading spinner)
    *  and then once we have the response, we wanna update the returned
    *  jsx code. We can use "State" to change what's visible on the screen.
    => import { useState } hook to the AllMeetups.js file.
    => creating const [isLoading, setIsLoading] in AllMeetupsPage() function.
    => adding setIsLoading(false) inside .then(data)
    => creating if (isLoading) and return a Loading... <p> until
    *  the Meetup data finishes fetching from the Database.
    => creating const [loadedMeetups, setLoadedMeetups] = useState([]);
    *  in AllMeetupsPage() function.
    => setLoadedMeetups(data) inside .then(data)
    => assign {loadedMeetups} to meetups= instead of {DUMMY_DATA}
    => this will assign the actual data fetched from the firebase database
    *  to the meetups prop.
    => Deleting the DUMMY_DATA
    => But now this code will execute an infinite loop.
    => because when we call the state updating function [, SetIsLoading]
    *  we tell react that it should re-execute the component function.
    => but we change state once we made our request inside .then(data)
    *  by setIsLoading(false)
    => it means the AllMeetupsPage() component function will execute once again
    => it means fetch() function will run again and we send another request.
    => once that done, we change the state again inside .then(data)
    *  by setIsLoading(false)
    => which means the AllMeetupsPage() component function will start
    *  executing once again. This will lead to make an infinite loop.

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,27 +1,34 @@
+import { useState } from "react";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
-
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  fetch(
+    "https://react-complete-meetup-app-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setIsLoading(false);
+      setLoadedMeetups(data);
+    });
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  // This Loading <p> will be shown while the meetups data is been fetched from the
+  // firebase database. And once the fetching is finished, .then((data))
+  // on above will set setIsLoading(false) and it will stop showing this Loading <p>
+  // showing and start showing the jsx elements below.
+
   return (
     <section>
       <h1>All Meetups</h1>
@@ -31,7 +38,8 @@ function AllMeetupsPage() {
           // This will return all title values of the properties of
           // DUMMY_DATA array as Unordered list items.
         })} */}
-        <MeetupList meetups={DUMMY_DATA} />
+        {/* <MeetupList meetups={DUMMY_DATA} /> */}
+        <MeetupList meetups={loadedMeetups} />
         {/* This meetups is the prop we use in MeetupList.js file */}
       </ul>
     </section>
