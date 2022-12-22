40381d104d7e411697addd3be6643e9476be9fa0
Avishka Indula <avishkainduladev@gmail.com>
2022-12-22 09:57:44 +0530

    More Context Usage
    => importing { useContext } and FavoritesContext to Favorites.js
    => useContext(FavoritesContext) in FavoritesPage() function.
    => import MeetupList to Favorites.js
    => add <MeetupList meetups={favoritesCtx.favorites} /> to Favorites.js
    => this meetups= prop will assign the favorites items to the
    *  MeetupList component and render them in the Favorites page.
    => creating let content; and creating a if block to see
    *  if (favoritesCtx.totalFavorites === 0) or not
    => now we can assign a <p> element to that let content telling
    *  No favorites if (favoritesCtx.totalFavorites === 0) else
    *  we can assign content = <MeetupList meetups={favoritesCtx.favorites} />
    => We've stored html elements in variables here. It's not strange.
    *  We can use jsx code anywhere a value is needed.
    => return() {content} inside the <section>
    => but these favorites will be gone right after we reload because
    *  we don't store that favorites in a database, instead we only
    *  store that in the memory. We're not going to do that here but
    *  it's something we could add to enhance this application.
    => Import { useContext } and FavoritesContext to MainNavigation.js
    => creating const favoritesCtx = useContext(FavoritesContext); in
    *  MainNavigation() function.
    => adding <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
    *  to My Favorites Link component.
    => This will now show a badge next to the My Favorites Navigation Link
    *  which has the total number of favorite items.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
@@ -5,8 +6,11 @@ import classes from "./MainNavigation.module.css";
 // the css classes we define on MainNavigation.modules.css  will
 // be properties of that object. We can attach those classes to
 // the jsx elements of this file now.
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -22,7 +26,12 @@ function MainNavigation() {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
